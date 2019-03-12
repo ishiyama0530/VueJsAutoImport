01000b59fb2f84361c58bafa3c6bdbcc87655fbd
Tamius Han <ziga.pirih@hotmail.com>
2019-03-12 20:51:58 +0100

    Fixed indentation, added vscode variables

--- src/vueFileImporter.ts
+++ src/vueFileImporter.ts
@@ -47,33 +47,44 @@ async function insertComponent(
   
   const config = vscode.workspace.getConfiguration()
   const hasNewLine = config.get<boolean>('vuejsAutoImport.insertOneComponentPerLine')
-  const indentationCount = config.get<boolean>('vuejsAutoImport.indentation')
+  const hasTrailingComma = config.get<boolean>('vuejsAutoImport.hasTrailingComma')
+  const indentationCount = config.get<number>('vuejsAutoImport.indentation') || 4
   
   const newLine = getEolString(document.eol)
   const indent = ' '.repeat(indentationCount)             // todo: allow switching between tabs and spaces
 
   const match = /components( )*:( )*{[\s\S]*?(?=})/.exec(text)
+  
   if (match && match.index > -1) {
     const insertIndex = match.index + match[0].length
     const insertPosition = document.positionAt(insertIndex)
-    if (match[0].trim().endsWith('{') || match[0].trim().endsWith(',')) {
+
+    // compute the string we're insterting
+    let componentString = '';
+
+    if (match[0].trim().endsWith('{') ) {
       // if no registed component (ex) components: {}
-      await editor.edit(edit => 
-        if (hasNewLine) {
-          edit.insert(insertPosition, `${newLine}${indent}${componentName}{$newLine}`
-        } else {
-          edit.insert(insertPosition, componentName)
-        }
-      })
+      if (hasNewLine) {
+        componentString = `${newLine}${indent}${componentName}${hasTrailingComma ? ',':''}${newLine}`
+      } else {
+        componentString = `${componentName}${hasTrailingComma?', ':''}`
+      }
     } else {
-      await editor.edit(edit => {
-        if (hasNewLine) {
-          edit.insert(insertPosition, `,${newLine}${indent}${componentName}`)
-        } else {
-          edit.insert(insertPosition, `,  ${componentName}`)
-        }
-      })
+      // Detect if there's a trailing comma. We only add  a comma if we don't detect
+      // a trailing comma.
+      const trailingCommaPresent = match[0].trim().endsWith(',');
+
+      if (hasNewLine) {
+        componentString = `${trailingCommaPresent?'':','}${newLine}${indent}${componentName}${hasTrailingComma ? ',':''}`
+      } else {
+        componentString = `${trailingCommaPresent?'':', '}${componentName}${hasTrailingComma ? ', ':''}`
+      }
     }
+
+    await editor.edit(edit => {
+      edit.insert(insertPosition, componentString);
+    });
+
   }
 }
 
